a3035cb356cfc6b48c92e0a9273bf579d02df690
Sanjit Soreng <sanjit.soreng@mindbowser.com>
2024-06-11 16:04:23 +0530

    Added fonts

--- template/src/screens/AuthScreens/Login/Login.styles.ts
+++ template/src/screens/AuthScreens/Login/Login.styles.ts
@@ -7,11 +7,24 @@ export const useStyles = () => {
     return StyleSheet.create({
         container: {
             flex: 1,
+            justifyContent: 'center',
             backgroundColor: themeColors.background,
         },
         logo: {
             height: 60,
             width: 60,
+        },
+        title: {
+            fontSize: 24,
+            marginBottom: 20,
+            textAlign: 'center',
+        },
+        input: {
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            marginBottom: 12,
+            paddingHorizontal: 10,
         },
     });
 };
--- template/src/screens/AuthScreens/Login/Login.tsx
+++ template/src/screens/AuthScreens/Login/Login.tsx
@@ -1,28 +1,56 @@
 import React from 'react';
-import {View, Text, TextInput, Button, Image} from 'react-native';
+import {View, TextInput, Button, Image, TouchableOpacity} from 'react-native';
 import useViewModel from './Login.viewmodel';
 import {useStyles} from './Login.styles';
 import Logo from '../../../assets/logosvg.svg';
+import {useTheme} from '../../../contexts/ThemeContext';
+import {Text} from '../../../components';
+
+const themes = ['dark', 'light', 'other'];
 
 const Login = () => {
     const {username, password, setPassword, setUsername, onSubmit, t} = useViewModel();
     const styles = useStyles();
+    const {setTheme} = useTheme();
+    const onPress = (value) => {
+        setTheme(value);
+    };
     return (
         <View style={styles.container}>
-            <Image source={require('../../../assets/logo.png')} />
+            <View style={{flexDirection: 'row', justifyContent: 'space-around', paddingVertical: 40}}>
+                {themes.map((item, index) => (
+                    <TouchableOpacity
+                        key={index + 'asd'}
+                        onPress={() => {
+                            onPress(item);
+                        }}
+                    >
+                        <Text>{item}</Text>
+                    </TouchableOpacity>
+                ))}
+            </View>
+            <View style={{flexDirection: 'row'}}>
+                <Image source={require('../../../assets/logo.png')} />
 
-            <Logo
-                height={60}
-                width={60}
-            />
-
+                <Logo
+                    height={60}
+                    width={60}
+                />
+            </View>
             <TextInput
+                style={styles.input}
+                placeholder={t('login.input.email.placeholder')}
                 value={username}
                 onChangeText={setUsername}
+                keyboardType="email-address"
+                autoCapitalize="none"
             />
             <TextInput
+                style={styles.input}
+                placeholder={t('login.input.password.placeholder')}
                 value={password}
                 onChangeText={setPassword}
+                secureTextEntry
             />
             <Button
                 title={t('login.button.title')}
